e71aa1e6e510a0ab38f6fdfff545cd60db7a55ef
zkq <1139090915@qq.com>
2026-07-17 00:58:04 +0000

    experiment: prepare guarded V10 sealed evaluation

---
+++ training/planner_grpo_seed_v1/scripts/finalize_planner_grpo_objective.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Version-neutral entry point for the sealed GRPO objective finalizer."""
+
+from finalize_planner_retry_safe_end_hard_v9_objective import main
+
+
+if __name__ == "__main__":
+    main()
